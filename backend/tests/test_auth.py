625029de0787aa6883a61b93906307530a92ae99
julianafalcaoo <154533645+julianafalcaoo@users.noreply.github.com>
2026-03-12 20:03:04 -0400

    feat: adiciona testes - check de API

---
+++ backend/tests/test_auth.py
@@ -0,0 +1,12 @@
+from auth_service import gerar_hash, verificar_senha, criar_token
+
+def test_hash_senha():
+    pass
+
+
+def test_hash_senha_errada():
+    pass
+
+
+def test_criar_token():
+    pass
